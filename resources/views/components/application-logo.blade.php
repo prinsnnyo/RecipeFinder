06ea77e665b2dab5420351d209c46c4fdb46aea3
kleyr567 <claireannebohol567@gmail.com>
2025-05-28 10:30:51 +0800

    fixed logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('RF-logo (2).png') }}" alt="RecipeFinder Logo" style="height: 230px; width: auto; display: block; margin: 10px auto 0 auto;" {{ $attributes }}>
+<img src="{{ asset('RF-logo.png') }}" alt="RecipeFinder Logo" style="height: 230px; width: auto; display: block; margin: 10px auto 0 auto;" {{ $attributes }}>
